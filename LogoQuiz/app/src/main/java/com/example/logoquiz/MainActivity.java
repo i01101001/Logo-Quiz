f5ff8ccfc80b84ff77a4a5fac78322e9c9ed3009
Ivan Gavino <ivan.gavino@kooapps.com>
2021-05-07 22:46:31 +0800

    Wire QuestionManager to MainActivity

--- LogoQuiz/app/src/main/java/com/example/logoquiz/MainActivity.java
+++ LogoQuiz/app/src/main/java/com/example/logoquiz/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.logoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        QuestionManager.instance.initialize();
+        QuestionManager.instance.randomizeQuestion();
+        displayQuestion(QuestionManager.instance.getCurrentQuestion());
+        bindButtons();
+    }
+    private void displayQuestion(Question question){
+        ImageView logoDisplay = findViewById(R.id.logoDisplay);
+        Button answer1 = findViewById(R.id.answer1);
+        Button answer2 = findViewById(R.id.answer2);
+        Button answer3 = findViewById(R.id.answer3);
+        Button answer4 = findViewById(R.id.answer4);
+        List<String> answersList = QuestionManager.instance.returnAnswersList(question);
+        logoDisplay.setImageResource(question.getLogoDrawableId());
+        answer1.setText(answersList.get(0));
+        answer2.setText(answersList.get(1));
+        answer3.setText(answersList.get(2));
+        answer4.setText(answersList.get(3));
+    }
+    private void onButtonClick(Button button){
+        if(QuestionManager.instance.isAnswerCorrect(button.getText().toString())){
+            QuestionManager.instance.randomizeQuestion();
+            displayQuestion(QuestionManager.instance.getCurrentQuestion());
+        }
+    }
+    private void bindButtons(){
+        ImageView logoDisplay = findViewById(R.id.logoDisplay);
+        Button answer1 = findViewById(R.id.answer1);
+        Button answer2 = findViewById(R.id.answer2);
+        Button answer3 = findViewById(R.id.answer3);
+        Button answer4 = findViewById(R.id.answer4);
+        logoDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                QuestionManager.instance.randomizeQuestion();
+                displayQuestion(QuestionManager.instance.getCurrentQuestion());
+            }
+        });
+        answer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonClick(answer1);
+            }
+        });
+        answer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonClick(answer2);
+            }
+        });
+        answer3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonClick(answer3);
+            }
+        });
+        answer4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonClick(answer4);
+            }
+        });
     }
 }
